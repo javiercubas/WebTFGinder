1b10bf14ec902a5da2ee36b889b21d93f241ab22
LCabelloC <lcabelloms@gmail.com>
2022-12-20 16:50:04 +0100

    Solution terminado

--- src/SolutionPropose.js
+++ src/SolutionPropose.js
@@ -25,7 +25,7 @@ const SolutionPropose = () => {
 
                 </div>
                 <div className="iconEmpresa">
-
+                    {/* <img src="assets/empresa.png" className="img"/>  */}
                 </div>
                 <div className="text">
                     <h3 className="textInfo">EMPRESA</h3>
@@ -33,13 +33,24 @@ const SolutionPropose = () => {
             </div>
             <div className="sectionText">
                 <div className="text">
-                    <p className="textP"></p>
+                    <p className="textP">Los estudiantes podrán entrar a la aplicación
+                    cuando la universidad les de permiso para comenzar a buscar un tutor. Estos
+                    indicarán 1 o 2 temas que le gustaría realizar y le aparecerán una serie de 
+                    profesores que tenga un tema relacionado con el suyo. </p>
                 </div>
                 <div className="text">
-                    <p className="textP"></p>
+                    <p className="textP">
+                    Los profesores recibirán peticiones de los estudiantes para ser tutorizados
+                    y podrán aceptarlas o rechazarlas. En caso de aceptarlas, comenzará lo comunicación 
+                    entre ambos para que el estudiante pueda comenzar a realizar su TFG.
+                    </p>
                 </div>
                 <div className="text">
-                    <p className="textP"></p>
+                    <p className="textP">
+                    Las empresas podrán entrar a la aplicación y ver los TFG que están realizando
+                    los estudiantes. En caso de que les interese alguno, podrán contactar con el estudiante y profesor
+                    para poder realizar el TFG en su empresa y potenciarlo en caso de una gran idea.
+                    </p>
                 </div>
             </div>
         </div>
