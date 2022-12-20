415cf99910bb3e1916008f1b95e884f05252f419
javiercubas <javiercubas2003@gmail.com>
2022-12-21 00:18:37 +0100

    update

--- src/SolutionPropose.js
+++ src/SolutionPropose.js
@@ -7,54 +7,45 @@ const SolutionPropose = () => {
             <div className="solution-section">
                 <div className="esquema">
                     <div className="iconME">
-                        <img src="assets/student.png" alt="Student" className="img-icons" />
+                        <img src="assets/student.png" alt="Student" className="img-icons img-student" />
                     </div>
-                    <div className="text">
-                        <h3 className="textInfo">TÚ</h3>
-                    </div>
+                    <h3 className="textInfo textTu">TÚ</h3>
                     <div className="row1">
 
                     </div>
-                    <div className="text">
-                        <h3 className="textInfo">TUTOR</h3>
-                    </div>
+                    <h3 className="textInfo textTeacher">TUTOR</h3>
+
                     <div className="iconUni">
-                        <img src="assets/teacher.png" alt="Teacher" className="img-icons" />
+                        <img src="assets/teacher.png" alt="Teacher" className="img-icons img-teacher" />
                     </div>
                     <div className="row2">
 
                     </div>
                     <div className="iconEmpresa">
-                        <img src="assets/empresa.png" alt="Empresa" className="img-icons" />
+                        <img src="assets/empresa.png" alt="Empresa" className="img-icons img-empresa" />
                     </div>
-                    <div className="text">
-                        <h3 className="textInfo">EMPRESA</h3>
-                    </div>
+                    <h3 className="textInfo textEmpresa">EMPRESA</h3>
                 </div>
                 <div className="sectionText">
-                    <div className="text">
-                        <p className="textP">Los estudiantes podrán entrar a la aplicación
-                            cuando la universidad les de permiso para comenzar a buscar un tutor. Estos
-                            indicarán 1 o 2 temas que le gustaría realizar y le aparecerán una serie de
-                            profesores que tenga un tema relacionado con el suyo. </p>
-                    </div>
-                    <div className="text">
-                        <p className="textP">
-                            Los profesores recibirán peticiones de los estudiantes para ser tutorizados
-                            y podrán aceptarlas o rechazarlas. En caso de aceptarlas, comenzará lo comunicación
-                            entre ambos para que el estudiante pueda comenzar a realizar su TFG.
-                        </p>
-                    </div>
-                    <div className="text">
-                        <p className="textP">
-                            Las empresas podrán entrar a la aplicación y ver los TFG que están realizando
-                            los estudiantes. En caso de que les interese alguno, podrán contactar con el estudiante y profesor
-                            para poder realizar el TFG en su empresa y potenciarlo en caso de una gran idea.
-                        </p>
-                    </div>
+                    <p className="textP">Los estudiantes podrán entrar a la aplicación
+                        cuando la universidad les de permiso para comenzar a buscar un tutor. Estos
+                        indicarán 1 o 2 temas que le gustaría realizar y le aparecerán una serie de
+                        profesores que tenga un tema relacionado con el suyo. </p>
+
+                    <p className="textP">
+                        Los profesores recibirán peticiones de los estudiantes para ser tutorizados
+                        y podrán aceptarlas o rechazarlas. En caso de aceptarlas, comenzará lo comunicación
+                        entre ambos para que el estudiante pueda comenzar a realizar su TFG.
+                    </p>
+                    <p className="textP">
+                        Las empresas podrán entrar a la aplicación y ver los TFG que están realizando
+                        los estudiantes. En caso de que les interese alguno, podrán contactar con el estudiante y profesor
+                        para poder realizar el TFG en su empresa y potenciarlo en caso de una gran idea.
+                    </p>
+
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
