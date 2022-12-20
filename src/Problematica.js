87ecc24d959f6c493ceb478f76fee33b6bf673a9
javiercubas <javiercubas2003@gmail.com>
2022-12-20 17:46:53 +0100

    update

--- src/Problematica.js
+++ src/Problematica.js
@@ -5,36 +5,34 @@ const Problematica = () => {
     return (
         <section id="problematica" className="problematica-div">
             <div className="section_problem">
-                <div className="title_section">
-                    <h2 className="title">¿QUÉ QUEREMOS RESOLVER?</h2>
-                </div>
+                <h2 className="title">¿QUÉ QUEREMOS RESOLVER?</h2>
                 <div className="content_section">
                     <div className="info2">
                         <div className="icon_info">
-                            <img src="./" className="img_info"/>
+                            <img src="assets/noHay.png" className="img_info" />
                         </div>
                         <div className="text_controll">
                             <h3 className="title_info">FALTA DE SINERGIA</h3>
                         </div>
-                        
+
                         <p className="text">En la actualidad, podemos encontrar dos problemas principales
-                                en la realización de los TFG. Por un lado, los estudiantes no encuentran un tutor
-                                adecuado por culpa de la restricción de los temas a la hora de elegir. Y por otro,
-                                el abandono de algunos profesores a la hora de tutorizar a los estudiantes.
-                            </p>
+                            en la realización de los TFG. Por un lado, los estudiantes no encuentran un tutor
+                            adecuado por culpa de la restricción de los temas a la hora de elegir. Y por otro,
+                            el abandono de algunos profesores a la hora de tutorizar a los estudiantes.
+                        </p>
                     </div>
                     <div className="info2">
                         <div className="icon_info">
-                            <img src='' className="img_info"/>
+                            <img src='assets/lowCharge.png' className="img_info" />
                         </div>
                         <div className="text_controll">
                             <h3 className="title_info">LENTITUD DEL PROCESO DE UNIÓN PROFE ALUMNO</h3>
                         </div>
                         <p className="text">Actualmente, los estudiantes tardan en comenzar su TFG
-                            entre 3-6 meses(desde el inicio del curso). Esto se debe a que los estudiantes 
+                            entre 3-6 meses(desde el inicio del curso). Esto se debe a que los estudiantes
                             deben esperar a que el profesor elija un tema de su interés y que el profesor
                             acepte ser tutor de dicho estudiante.
-                            </p>
+                        </p>
                     </div>
                 </div>
             </div>
