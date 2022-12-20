57424fa67469b8a6ac01f99c3f1dc37819c2b8ac
LCabelloC <lcabelloms@gmail.com>
2022-12-20 01:20:37 +0100

    Añado la pestaña que queremos resolver, faltan img.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Caracteristicas from './Caracteristicas';
 import Header from './Header';
 import Inicio from './Inicio';
+import Problematica from './Problematica';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Inicio/>
-      <Caracteristicas/>
+      {/* <Caracteristicas/> */}
+      <Problematica/>
     </div>
   );
 }
