96cac8b503c2166031e77a27afe9ccb74f2c29c3
LCabelloC <lcabelloms@gmail.com>
2022-12-21 03:08:45 +0100

    Arreglados algunos fallos de la pagina.

--- src/MiTFG.js
+++ src/MiTFG.js
@@ -14,21 +14,38 @@ const MiTFG = () => {
                     <h3 className="index-mitfg">01</h3>
                     <div className='right-row-mitfg'>
                         <h4 className="title-mitfg">Nuestro compromiso</h4>
-                        <p className="subtitle-mitfg">La finalidad de la app es el ayudar a todo tipo de estudiante a encontrar el TFG perfecto para su entrega  y así poder mejorar la relación estudiante-profesor durante todo su proceso de desarrollo.</p>
+                        <p className="subtitle-mitfg">
+                            Nuestro compromiso es que todos los estudiantes puedan
+                            realizar su TFG de una manera más sencilla y eficiente. Con fechas de entregas 
+                            menos agresivas y con una posible empresa detrás que pueda ayudar al desarrollo.  
+
+                        </p>
                     </div>
                 </div>
                 <div className="row-mitfg-div">
                     <h3 className="index-mitfg">02</h3>
                     <div className='right-row-mitfg'>
                         <h4 className="title-mitfg">Nuestros objetivos</h4>
-                        <p className="subtitle-mitfg">Conectar a las personas y ayudarlas a encontrar a su profesor ideal. De esta manera facilitar la realización el TFG produciendo trabajos de mayor calidad y una mejor experiencia a a hora de realizarlo.</p>
+                        <p className="subtitle-mitfg">
+                            El principal objetivo es que todos los estudiantes realicen su TFG de una manera
+                            más sencilla y eficiente, para ello hemos creado una plataforma que permite 
+                            a los estudiantes encontrar el TFG perfecto para ellos, y a los profesores
+                            encontrar a los estudiantes perfectos para ellos. A los cuales se podrán unir las 
+                            empresas que quieran colaborar con el desarrollo del TFG.
+                        </p>
                     </div>
                 </div>
                 <div className="row-mitfg-div">
                     <h3 className="index-mitfg">03</h3>
                     <div className='right-row-mitfg'>
                         <h4 className="title-mitfg">La privacidad</h4>
-                        <p className="subtitle-mitfg">Queremos dar una sensación de total seguridad e impunidad ante todo posible ataque cibernético para así impedir pérdidas de datos confidenciales de la empresa o de los datos de los propios clientes.</p>
+                        <p className="subtitle-mitfg">
+                            En TFGinder nos preocupamos por la privacidad de nuestros usuarios, por ello
+                            hemos creado una plataforma que permite a los usuarios controlar qué datos
+                            quieren compartir con los demás usuarios. Además, los datos de los usuarios
+                            no se compartirán con terceros.
+
+                        </p>
                     </div>
                 </div>
             </div>
